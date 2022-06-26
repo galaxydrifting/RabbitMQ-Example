008065fb27cc9ff66b447baf11dff7aabdb010ff
drift2galaxy <drift2galaxy@gmail.com>
2022-06-26 13:47:41 +0800

    first commit

---
+++ admin/consumer.py
@@ -0,0 +1,34 @@
+import pika, json, os, django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin.settings")
+django.setup()
+
+from products.models import Product
+
+params = pika.URLParameters('amqps://nrlmqfuz:dpN-kI883VX-j6oW002E4sfPIoDLFrgX@cougar.rmq.cloudamqp.com/nrlmqfuz')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin')
+
+
+def callback(ch, method, properties, body):
+    print('Received in admin')
+    id = json.loads(body)
+    print(id)
+    product = Product.objects.get(id=id)
+    product.likes = product.likes + 1
+    product.save()
+    print('Product likes increased!')
+
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+
+print('Started Consuming')
+
+channel.start_consuming()
+
+channel.close()
